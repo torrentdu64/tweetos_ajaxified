b687f51a2f6fd9d3bda984db61cf5db3acf85d11
torrent benoit <torrentdu64@gmail.com>
2017-01-12 18:06:13 +0100

    add @review = @wall.reviews.build(reviews_params) to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,12 @@
 class ReviewsController < ApplicationController
   def create
     @wall = Wall.find(params[:wall_id])
-
-    @review = Review.new(reviews_params)
-    @review.wall = @wall
+    @review = @wall.reviews.build(reviews_params)
+    # @review = Review.new(reviews_params)
+    # @review.wall = @wall
+    @reviews = @wall.reviews.select do |review|
+      review.persisted?
+    end
     if @review.save
       redirect_to wall_path(@wall)
     else
