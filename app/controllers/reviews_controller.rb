656b994746f323afa598933d8aa96afd228e2b3f
torrent benoit <torrentdu64@gmail.com>
2017-01-12 17:25:59 +0100

    add reviews create controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,17 @@
 class ReviewsController < ApplicationController
   def create
+    @wall = Wall.find(params[:wall_id])
+    @review = @wall.reviews.build(reviews_params)
+    if @review.save
+      redirect_to wall_path(@wall)
+    else
+      render 'walls/show'
+    end
+  end
+
+  private
+
+  def reviews_params
+    params.require(:review).permit(:content)
   end
 end
