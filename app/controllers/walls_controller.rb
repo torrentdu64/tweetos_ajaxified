ba5156fa8b6caea1cb70960e5b9cb1cc15e42fab
torrent benoit <torrentdu64@gmail.com>
2017-01-12 17:57:34 +0100

    replace @reviews = @wall.reviews.select do |review| review.persisted? to index at show

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -2,13 +2,13 @@ class WallsController < ApplicationController
 
   def index
     @walls = Wall.all
-    @reviews = @wall.reviews.select do |review|
-      review.persisted?
-    end
   end
 
   def show
     @wall = Wall.find(params[:id])
     @review = Review.new
+    @reviews = @wall.reviews.select do |review|
+      review.persisted?
+    end
   end
 end
