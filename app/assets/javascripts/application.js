b034b3329386e338f2e43c2d45942318ed4006e9
torrent benoit <torrentdu64@gmail.com>
2017-01-16 17:16:33 +0100

    add javscript to image wall view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,26 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).ready(function() {
+
+  $( "#image" ).append('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>').fadeIn( 400 );
+  $( "#image" ).delay( 3000 );
+  $( "#image" ).slideUp( 800);
+  // $("#image-final").append('<img class="img" src="http://lorempixel.com/740/680/#{@wall.photo}/" alt="Nightlife">').delay(200000).queue(function (next) {
+  //   next();
+  // });
+  function someFunction() {
+    for (var i = 0; i < 10; i++) {
+        setTimeout(function(){
+          addElement();
+        }, 100000 * i);
+    }
+  }
+
+  function addElement() {
+      $("#image-final").append('<img class="img" src="http://lorempixel.com/740/680/#{@wall.photo}/" alt="Nightlife">');
+  }
+  someFunction();
+});
